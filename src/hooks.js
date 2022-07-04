bd42fdaec1fa476150acff801b95d79d8649cdfb
Dirk Robinson <tiaan.robinson@gmail.com>
2022-07-04 08:59:03 +0200

    Update

--- src/hooks.js
+++ src/hooks.js
@@ -1,9 +1,28 @@
-//--to resolve the request and return the result---
-export const handle = async ({ request, resolve }) => {
-    const response = await resolve(request)
+//Hooks (handle, handleError and getSession)
+//--You haveto resolve the request and return the result---
+//handle runs on all requests
+
+// export const handle = async ({ request, resolve }) => {
+//    request.locals.userName = "Tiaan"
+//     const response = await resolve(request)
+//      response.auth = "good";
+//
+//     return {
+//         ...reponse,
+//      headers: {
+//          ...response.headers,    
+//          "hello-world": "hey programmer"
+//          },
+//     };
+// };
+
+//Updating apps 
+
+export const handle = async ({event, resolve}) => {
+    const response = await resolve(event)
 
     return {
-        ...reponse,
+        ...response,
     };
 };
 
